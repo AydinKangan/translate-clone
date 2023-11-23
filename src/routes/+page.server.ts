c9ba42014f19614c7e4c08db3ddcd184dc581a1d
Aydin Elhan <200030355@student.kangan.edu.au>
2023-11-23 11:17:51 +1100

    data sending and recieving

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,32 +1,43 @@
 import type { Actions } from '@sveltejs/kit';
-import {X_RAPID_API_KEY} from '$env/static/private';
+
+type Note = {
+	translation: string;
+};
 
 export const actions = {
-	translate: async (event) => {
-		const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
-		const options = {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/x-www-form-urlencoded',
-				'Accept-Encoding': 'application/gzip',
-				'X-RapidAPI-Key': X_RAPID_API_KEY,
-				'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-			},
-			body: new URLSearchParams({
-				q: 'Hello, world!',
-				target: 'es',
-				source: 'en'
-			})
-		};
+	translate: async ({request}) => {
+		const data = Object.fromEntries(await request.formData()) as Note;
+		console.log(data)
+		
 
-		try {
-			const response = await fetch(url, options);
-			const result = await response.text();
-			const translation = JSON.parse(result).data.translations[0].translatedText;
-            return {translation}
-		} catch (error) {
-			console.error(error);
-		}
-
+		return "hellow server"
 	}
 } satisfies Actions;
+
+
+// import {X_RAPID_API_KEY} from '$env/static/private';
+
+// const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+		// const options = {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'content-type': 'application/x-www-form-urlencoded',
+		// 		'Accept-Encoding': 'application/gzip',
+		// 		'X-RapidAPI-Key': X_RAPID_API_KEY,
+		// 		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+		// 	},
+		// 	body: new URLSearchParams({
+		// 		q: 'Hello, world!',
+		// 		target: 'es',
+		// 		source: 'en'
+		// 	})
+		// };
+
+		// try {
+		// 	const response = await fetch(url, options);
+		// 	const result = await response.text();
+		// 	const translation = JSON.parse(result).data.translations[0].translatedText;
+        //     return {translation}
+		// } catch (error) {
+		// 	console.error(error);
+		// }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,69 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { enhance } from '$app/forms';
 
-	let translation: string = 'Hello World';
+	let translation: string = '';
+	export let data: any;
 
-	
+	$: {
+		if (data) {
+			console.log(data);
+		}
+	}
 
+	const handleSubmit = async (event: Event) => {
+		event.preventDefault();
+		const form = event.target as HTMLFormElement;
+		const formData = new FormData(form);
 
-		const translateButtonClickHandler = async (event:any) => {
-			let formData = new FormData();
-			formData.append('q', translation);
-
-			console.log(formData);
-
-			let response = await fetch('?/translate' , {
+		try {
+			const response = await fetch('?/translate', {
 				method: 'POST',
-				headers: {
-					'Content-Type': 'multipart/form-data'
-				},
 				body: formData
 			});
 
-			console.log(response);
+			if (response.ok) {
+				const result = await response.json();
+				const jsonData = JSON.parse(result.data);
+				const resultString = jsonData[0];
+				console.log(resultString)
+				translation = resultString;
+			} else {
+				console.error('Translation request failed');
+			}
+		} catch (error) {
+			console.error('Error during translation request', error);
 		}
+	};
 </script>
 
-<div>
-	<div class="flex flex-row p-[3rem] justify-evenly">
-		<div class="w-[35%] h-[8rem]">
-			<span>Textarea</span>
-			<textarea
-				class="box textarea h-full w-full p-3"
-				rows="4"
-				placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-			/>
-		</div>
-		<button></button>
-
-		<div class="w-[35%] h-[8rem]">
-			<span>Textarea</span>
-			<div class="box bg-surface-700 border-surface-500 border-[1px] w-full h-full p-3">
-				{#if translation}
-					<p>{translation}</p>
-				{:else}
-					<p class="text-gray-500">Translation</p>
-				{/if}
+<div class="flex flex-row justify-evenly p-[4rem]">
+	<form class="w-[35%]" on:submit={handleSubmit}>
+		<label>
+			Translation
+			<div class="h-[8rem]">
+				<textarea
+					class="box textarea h-full w-full p-3"
+					rows="4"
+					placeholder="Enter translation."
+					name="translation"
+				></textarea>
 			</div>
+		</label>
+
+		<div class="flex justify-end pt-[2rem]">
+			<button type="submit" class="bg-primary-500 text-white p-3 rounded-md">Translate</button>
 		</div>
-	</div>
-	<div class="flex justify-center pt-[2rem]">
-		<button on:click={translateButtonClickHandler} class="bg-primary-500 text-white p-3 rounded-md">Translate</button>
+	</form>
+
+	<div class="w-[35%] h-[8rem]">
+		<span>Result</span>
+		<div class="box bg-surface-700 border-surface-500 border-[1px] w-full h-full p-3">
+			{#if translation}
+				<p>{translation}</p>
+			{:else}
+				<p class="text-gray-500">Translation</p>
+			{/if}
+		</div>
 	</div>
 </div>
 
