4ae0a479b7cf274739a0028f6900e322cc329cc5
Aydin Elhan <200030355@student.kangan.edu.au>
2023-11-23 12:14:18 +1100

    translation working for turkish

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,43 +1,39 @@
 import type { Actions } from '@sveltejs/kit';
+import { X_RAPID_API_KEY } from '$env/static/private';
 
 type Note = {
 	translation: string;
 };
 
 export const actions = {
-	translate: async ({request}) => {
+	translate: async ({ request }) => {
 		const data = Object.fromEntries(await request.formData()) as Note;
-		console.log(data)
-		
+		console.log(data.translation);
 
-		return "hellow server"
+		const url = 'https://google-translate113.p.rapidapi.com/api/v1/translator/text';
+
+		const options = {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/x-www-form-urlencoded',
+
+				'X-RapidAPI-Key': X_RAPID_API_KEY,
+				'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+			},
+			body: new URLSearchParams({
+				from: 'auto',
+				to: 'tr',
+				text: data.translation
+			})
+		};
+
+		try {
+			const response = await fetch(url, options);
+			const result = await response.text();
+			const translation = JSON.parse(result).trans;
+			return translation;
+		} catch (error) {
+			console.error(error);
+		}
 	}
 } satisfies Actions;
-
-
-// import {X_RAPID_API_KEY} from '$env/static/private';
-
-// const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
-		// const options = {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'content-type': 'application/x-www-form-urlencoded',
-		// 		'Accept-Encoding': 'application/gzip',
-		// 		'X-RapidAPI-Key': X_RAPID_API_KEY,
-		// 		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-		// 	},
-		// 	body: new URLSearchParams({
-		// 		q: 'Hello, world!',
-		// 		target: 'es',
-		// 		source: 'en'
-		// 	})
-		// };
-
-		// try {
-		// 	const response = await fetch(url, options);
-		// 	const result = await response.text();
-		// 	const translation = JSON.parse(result).data.translations[0].translatedText;
-        //     return {translation}
-		// } catch (error) {
-		// 	console.error(error);
-		// }
